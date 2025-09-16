ab0f87506c57ce965e5a630cd0119920ad4480a8
Sam Schams <schams@digineo.de>
2025-09-16 13:54:05 +0200

    Update Node.js version in .tool-versions and modify Supabase access token variable in api.ts for consistency.

--- lib/api.ts
+++ lib/api.ts
@@ -3,15 +3,15 @@ import { createClient } from "@supabase/supabase-js";
 
 if (
   !import.meta.env.VITE_SUPABASE_URL ||
-  !import.meta.env.VITE_SUPABASE_ACCESS_TOKEN
+  !import.meta.env.VITE_SUPABASE_PUBLIC_ACCESS_TOKEN
 ) {
   throw new Error(
-    "VITE_SUPABASE_URL and VITE_SUPABASE_ACCESS_TOKEN must be set in .env",
+    "VITE_SUPABASE_URL and VITE_SUPABASE_PUBLIC_ACCESS_TOKEN must be set in .env",
   );
 }
 const supabase = createClient(
   import.meta.env.VITE_SUPABASE_URL,
-  import.meta.env.VITE_SUPABASE_ACCESS_TOKEN,
+  import.meta.env.VITE_SUPABASE_PUBLIC_ACCESS_TOKEN,
 );
 
 export default supabase;
